d69e0c383494886caad8eed2da327818454a97b6
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-01 15:30:33 +0530

    Added Project File

---
+++ src/main/java/StackandQueue/Bridgelabz/com/App.java
@@ -0,0 +1,13 @@
+package StackandQueue.Bridgelabz.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
