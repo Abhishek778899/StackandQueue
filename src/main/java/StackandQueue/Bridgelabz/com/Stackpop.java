8a402312fe4819cf01f3db94d2bc6e6c7d1e0532
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-01 15:35:53 +0530

    Stackpop

---
+++ src/main/java/StackandQueue/Bridgelabz/com/Stackpop.java
@@ -0,0 +1,37 @@
+package StackandQueue.Bridgelabz.com;
+
+public class Stackpop {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack and Queue");
+
+		System.out.println("UC1: Pushing elements 70,30,56 into Stack : ");
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		System.out.println("Printing elements of Stack : ");
+		linkedList.display();
+
+		System.out.println("UC2: Peeking and Popping elements of Stack : ");
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.display();
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.display();
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.display();
+
+	}
+}
