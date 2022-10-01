49f99337e1e2ec7ee4cbfad33001152d5d191497
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-01 16:16:20 +0530

    Stackdqueue

---
+++ src/main/java/StackandQueue/Bridgelabz/com/Stackdequeue.java
@@ -0,0 +1,58 @@
+package StackandQueue.Bridgelabz.com;
+
+public class Stackdequeue {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack and Queue");
+
+		System.out.println("UC1: Pushing elements 70,30,56 into Stack : ");
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		System.out.println("Printing elements of Stack : ");
+		linkedList.displayStack();
+
+		System.out.println("UC2: Peeking and Popping elements of Stack : ");
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.displayStack();
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.displayStack();
+		System.out.println("Peeking top of stack : ");
+		linkedList.peek();
+		System.out.println("Popping top of stack... ");
+		linkedList.pop();
+		System.out.println("Printing elements of Stack : ");
+		linkedList.displayStack();
+
+		System.out.println("UC3: Enqueueing elements 56,30,70 into Queue : ");
+		LinkedList<Integer> linkedList2 = new LinkedList<>();
+		linkedList2.enqueue(56);
+		linkedList2.enqueue(30);
+		linkedList2.enqueue(70);
+		System.out.println("Printing elements of Queue : ");
+		linkedList2.displayQueue();
+
+		System.out.println("UC4: Dequeueing elements of Queue : ");
+		System.out.println("Dequeueing front of queue... ");
+		linkedList2.dequeue();
+		System.out.println("Printing elements of Queue : ");
+		linkedList2.displayQueue();
+		System.out.println("Dequeueing front of queue... ");
+		linkedList2.dequeue();
+		System.out.println("Printing elements of Queue : ");
+		linkedList2.displayQueue();
+		System.out.println("Dequeueing front of queue... ");
+		linkedList2.dequeue();
+		System.out.println("Printing elements of Queue : ");
+		linkedList2.displayQueue();
+	}
+}
